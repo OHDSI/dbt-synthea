025c6897de4c53aa4f3b34635aa98d47cb273eae
adam bouras <adambouras1@gmail.com>
2024-02-17 23:22:06 -0500

    use jinja macro to make all columns lower case

--- synthea_omop_etl/models/base/synthea_patients.sql
+++ synthea_omop_etl/models/base/synthea_patients.sql
@@ -1,11 +1,19 @@
+-- Returns a list of the columns from a relation, so you can then iterate in a for loop
+{% set column_names = 
+    dbt_utils.get_filtered_columns_in_relation( source('synthea', 'patients') ) 
+%}
+
+
 WITH patients AS (
 
-    SELECT
-        "Id" AS patient_id,
-        "BIRTHDATE" AS patient_birth_date
-    FROM {{ ref('patients') }}
+    SELECT  *
+    FROM {{ source('synthea','patients' ) }}
+) 
 
-)
+select  
+{% for column_name in column_names %}
+    "{{ column_name }}" as {{ column_name | lower }}
+    {% if not loop.last %},{% endif %}
+{% endfor %}
 
-SELECT *
 FROM patients
