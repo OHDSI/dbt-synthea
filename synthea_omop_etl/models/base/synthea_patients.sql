82c20ecd05886f55e45fdb88f4a3cf0687d1770f
adam bouras <adambouras1@gmail.com>
2024-02-18 18:48:16 -0500

    add jinja code to traverse columns dynamically, and updated the schema.yml to include sources

--- synthea_omop_etl/models/base/synthea_patients.sql
+++ synthea_omop_etl/models/base/synthea_patients.sql
@@ -1,11 +1,25 @@
+-- Returns a list of the columns from a relation, so you can then iterate in a for loop
+{% set column_names = 
+    dbt_utils.get_filtered_columns_in_relation( source('synthea', 'patients') ) 
+%}
+
+
 WITH patients AS (
 
-    SELECT
-        "Id" AS patient_id,
-        "BIRTHDATE" AS patient_birth_date
-    FROM {{ ref('patients') }}
+    SELECT  *
+    FROM {{ source('synthea','patients' ) }}
+) 
 
-)
+select  
+{% for column_name in column_names %}
+        {% if column_name == "Id" %}
+            "{{ column_name }}" as patient_id
+        {% elif column_name ==  "BIRTHDATE" %}
+            "{{ column_name }}" as  patient_birth_date
+        {% else %}
+            "{{ column_name }}" as {{ column_name | lower }}
+        {% endif %}
+    {% if not loop.last %},{% endif %}
+{% endfor %}
 
-SELECT *
 FROM patients
