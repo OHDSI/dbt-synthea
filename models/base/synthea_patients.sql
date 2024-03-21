57044ef19148a770001e9891265a3fb039d49abe
Katy Sadowski <katy.h.sadowski@gmail.com>
2024-03-21 14:22:36 -0600

    remove extraneous project folder

---
+++ models/base/synthea_patients.sql
@@ -0,0 +1,30 @@
+-- Returns a list of the columns from a relation, so you can then iterate in a for loop
+{% set column_names = 
+    dbt_utils.get_filtered_columns_in_relation( source('synthea', 'patients') ) 
+%}
+
+
+WITH cte_patients_lower AS (
+
+    SELECT  
+
+        {% for column_name in column_names %}
+            "{{ column_name }}" as {{ column_name | lower }}
+        {% if not loop.last %},{% endif %}
+        {% endfor %}
+        
+    FROM {{ source('synthea','patients') }}
+) 
+, cte_patients AS (
+
+    SELECT 
+        id AS patient_id
+        , birthdate AS patient_birth_date
+        , race AS patient_race
+        , ethnicity AS patient_ethnicity
+    FROM cte_patients_lower
+
+)
+
+SELECT  * 
+FROM cte_patients
