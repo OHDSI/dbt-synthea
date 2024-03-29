00d9ca049d36114e72b72289b51192994017efc4
Katy Sadowski <katy.h.sadowski@gmail.com>
2024-03-28 20:05:03 -0600

    complete addition of patients stg table

--- models/staging/synthea/stg_synthea__patients.sql
+++ models/staging/synthea/stg_synthea__patients.sql
@@ -21,8 +21,29 @@ WITH cte_patients_lower AS (
     SELECT 
         id AS patient_id
         , birthdate AS patient_birth_date
+        , deathdate AS patient_death_date
+        , ssn AS patient_ssn
+        , drivers AS patient_drivers_license_number
+        , passport AS patient_passport_number
+        , prefix AS patient_prefix
+        , first AS patient_first_name
+        , last AS patient_last_name
+        , suffix AS patient_suffix
+        , maiden AS patient_maiden_name
+        , marital AS patient_marital_status
         , race AS patient_race
         , ethnicity AS patient_ethnicity
+        , gender AS patient_gender
+        , birthplace AS patient_birthplace
+        , address AS patient_address
+        , city AS patient_city
+        , state AS patient_state
+        , county AS patient_county
+        , zip AS patient_zip
+        , lat AS patient_latitude
+        , lon AS patient_longitude
+        , healthcare_expenses AS patient_healthcare_expenses
+        , healthcare_coverage AS patient_healthcare_coverage
     FROM cte_patients_lower
 
 )
