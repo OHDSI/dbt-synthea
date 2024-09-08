f2d7623f3d84b1e1c39703903597e3617c2c8ee8
Katy Sadowski <katy.sadowski@gmail.com>
2024-09-08 19:40:44 -0400

    update hardcoded cdm_source entries

--- models/omop/cdm_source.sql
+++ models/omop/cdm_source.sql
@@ -1,14 +1,14 @@
 SELECT
-    '@cdm_source_name' AS cdm_source_name
-    , '@cdm_source_abbreviation' AS cdm_source_abbreviation
-    , '@cdm_holder' AS cdm_holder
-    , '@source_description' AS source_description
+    'dbt-synthea' AS cdm_source_name
+    , 'dbt-synthea' AS cdm_source_abbreviation
+    , 'OHDSI' AS cdm_holder
+    , 'An OMOP CDM derived from a Synthea dataset using dbt.' AS source_description
     , 'https://synthetichealth.github.io/synthea/' AS source_documentation_reference
-    , 'https://github.com/OHDSI/ETL-Synthea' AS cdm_etl_reference
+    , 'https://github.com/OHDSI/dbt-synthea' AS cdm_etl_reference
     , current_date AS source_release_date
     , current_date AS cdm_release_date
-    , '@cdm_version' AS cdm_version
+    , '5.4' AS cdm_version
     , vocabulary_version
-    , 75626 AS cdm_version_concept_id
+    , 798878 AS cdm_version_concept_id
 FROM {{ ref('stg_vocabulary__vocabulary') }}
 WHERE vocabulary_id = 'None'
