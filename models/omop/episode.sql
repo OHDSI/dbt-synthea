c81ddc0419ca5e70c9253a45c545006c6451e590
Katy Sadowski <katy.sadowski@gmail.com>
2024-09-28 20:58:20 -0400

    remove omop seeds, rename vocab seeds

--- models/omop/episode.sql
+++ models/omop/episode.sql
@@ -1,1 +1,15 @@
-SELECT * FROM {{ ref('stg_omop__episode') }}
+SELECT
+    CAST(NULL AS INTEGER) AS episode_id
+    , CAST(NULL AS INTEGER) AS person_id
+    , CAST(NULL AS INTEGER) AS episode_concept_id
+    , CAST(NULL AS DATE) AS episode_start_date
+    , CAST(NULL AS TIMESTAMP) AS episode_start_datetime
+    , CAST(NULL AS DATE) AS episode_end_date
+    , CAST(NULL AS TIMESTAMP) AS episode_end_datetime
+    , CAST(NULL AS INTEGER) AS episode_parent_id
+    , CAST(NULL AS INTEGER) AS episode_number
+    , CAST(NULL AS INTEGER) AS episode_object_concept_id
+    , CAST(NULL AS INTEGER) AS episode_type_concept_id
+    , CAST(NULL AS VARCHAR) AS episode_source_value
+    , CAST(NULL AS INTEGER) AS episode_source_concept_id
+WHERE 1 = 0
--- models/omop/note.sql
+++ models/omop/note.sql
@@ -1,1 +1,18 @@
-SELECT * FROM {{ ref('stg_omop__note') }}
+SELECT
+    CAST(NULL AS INTEGER) AS note_id
+    , CAST(NULL AS INTEGER) AS person_id
+    , CAST(NULL AS DATE) AS note_date
+    , CAST(NULL AS TIMESTAMP) AS note_datetime
+    , CAST(NULL AS INTEGER) AS note_type_concept_id
+    , CAST(NULL AS INTEGER) AS note_class_concept_id
+    , CAST(NULL AS VARCHAR) AS note_title
+    , CAST(NULL AS VARCHAR) AS note_text
+    , CAST(NULL AS INTEGER) AS encoding_concept_id
+    , CAST(NULL AS INTEGER) AS language_concept_id
+    , CAST(NULL AS INTEGER) AS provider_id
+    , CAST(NULL AS INTEGER) AS visit_occurrence_id
+    , CAST(NULL AS INTEGER) AS visit_detail_id
+    , CAST(NULL AS VARCHAR) AS note_source_value
+    , CAST(NULL AS INTEGER) AS note_event_id
+    , CAST(NULL AS INTEGER) AS note_event_field_concept_id
+WHERE 1 = 0
