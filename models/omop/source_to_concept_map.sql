5ff2aaa26ee82959f50d3b3fb56320e69a638d40
Lawrence Adams <lawrencetadams@googlemail.com>
2024-11-03 18:56:29 +0000

    fix: create empty source_to_concept_map model
     - used insted of seeding an empty csv file
     - enables use of 'BYOData' to the project with postgres/snowflake/etc

--- models/omop/source_to_concept_map.sql
+++ models/omop/source_to_concept_map.sql
@@ -1,1 +1,11 @@
-SELECT * FROM {{ ref('stg_vocabulary__source_to_concept_map') }}
+SELECT
+    cast(null AS int) AS source_code
+    , cast(null AS varchar) AS source_concept_id
+    , cast(null AS int) AS source_vocabulary_id
+    , cast(null AS varchar) AS source_code_description
+    , cast(null AS int) AS target_concept_id
+    , cast(null AS varchar) AS target_vocabulary_id
+    , cast(null AS date) AS valid_start_date
+    , cast(null AS date) AS valid_end_date
+    , cast(null AS varchar(1)) AS invalid_reason
+WHERE false
