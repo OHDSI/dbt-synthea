a4ee3b4d6d395a1d2a6d30302519b58cdd21f0b6
Katy Sadowski <katy.sadowski@gmail.com>
2025-06-15 10:07:01 -0400

    csv refactor

--- scripts/python/generate_dbt_yaml.py
+++ scripts/python/generate_dbt_yaml.py
@@ -1,46 +1,54 @@
 #!/usr/bin/env  -S uv run --script
 # /// script
 # requires-python = ">=3.12"
-# dependencies = [ "beautifulsoup4", "ruamel-yaml", "requests" ]
+# dependencies = [ "ruamel-yaml", "requests" ]
 # ///
 
 """
 A script to generate dbt YAML files from the OMOP CDM documentation
-Requires `BeautifulSoup4`, `ruamel.yaml`, and `requests` to be installed.
+Requires `ruamel.yaml`, and `requests` to be installed.
 If you have uv installed this will be done automatically.
 
 If you make this file executable you should also be able to run it directly using
 just the file path/name and the output directory path. e.g:
 ./generate_dbt_yaml.py <output_dir>
 
-CDM version used is CDM 5.4 which corresponds to the url below.
-https://raw.githubusercontent.com/OHDSI/CommonDataModel/refs/heads/main/docs/cdm54.html
+CDM version used is CDM 5.4 which corresponds to the urls referenced below.
 
-If you wish to use a different version, either pass the url in as the --source argument
+This script processes two CSV files:
+
+1. Field-level documentation: Contains details about individual fields in the OMOP CDM tables.
+   Default URL: https://raw.githubusercontent.com/OHDSI/CommonDataModel/refs/heads/main/inst/csv/OMOP_CDMv5.4_Field_Level.csv
+
+2. Table-level documentation: Contains descriptions for each OMOP CDM table.
+   Default URL: https://raw.githubusercontent.com/OHDSI/CommonDataModel/refs/heads/main/inst/csv/OMOP_CDMv5.4_Table_Level.csv
+
+The script generates dbt YAML files for each table, including field-level details and table descriptions.
+
+If you wish to use a different CDM version, either pass the url in as the --source argument
 or download the html file and pass the path in as --source.
 For example using wget <url>.
 """
 
 import argparse
+import csv
 import tempfile
 from dataclasses import dataclass
 from pathlib import Path
 from urllib.parse import ParseResult, urlparse
 
 import requests
-from bs4 import BeautifulSoup
-from bs4._typing import _AtMostOneElement  # pyright: ignore[reportPrivateUsage]
-from bs4.element import NavigableString, Tag
 from ruamel.yaml import YAML
-from ruamel.yaml.comments import CommentedSeq
 
-default_source_url = "https://raw.githubusercontent.com/OHDSI/CommonDataModel/refs/heads/main/docs/cdm54.html"
+default_field_source_url = "https://raw.githubusercontent.com/OHDSI/CommonDataModel/refs/heads/main/inst/csv/OMOP_CDMv5.4_Field_Level.csv"
+default_table_source_url = "https://raw.githubusercontent.com/OHDSI/CommonDataModel/refs/heads/main/inst/csv/OMOP_CDMv5.4_Table_Level.csv"
 
 
 @dataclass
 class OmopDocumentationContainer:
     """Object to store how the CDM docs express each column"""
 
+    table_name: str
     cdm_field: str
     user_guide: str
     etl_conventions: str
@@ -50,6 +58,7 @@ class OmopDocumentationContainer:
     foreign_key: bool
     foreign_key_table: str
     foreign_key_domain: str
+    foreign_key_class: str
 
 
 @dataclass
@@ -57,7 +66,8 @@ class CliArgs:
     """A dataclass to ensure correct typing of command line arguments"""
 
     output_dir: Path = Path()
-    source: str = default_source_url
+    field_source: str = default_field_source_url
+    table_source: str = default_table_source_url
     overwrite: bool = False
 
 
@@ -79,7 +89,7 @@ def download_url_to_temp_file(url: str, output_dir: Path) -> Path:
         return Path(tmp_file.name)
 
 
-def parse_cli_arguments() -> tuple[Path, Path]:
+def parse_cli_arguments() -> tuple[Path, str, str]:
     """
     Parse command line arguments.
 
@@ -98,12 +108,20 @@ def parse_cli_arguments() -> tuple[Path, Path]:
         "output_dir", type=Path, help="Path to the output directory"
     )
 
-    # Common data model url.
+    # Common data model field-level source url.
     _ = parser.add_argument(
-        "--source",
-        "-s",
+        "--field-source",
+        "-fs",
         type=str,
-        help=f'Path or url to source html. If none provided defaults to:\n"{default_source_url}"',
+        help=f'Path or url to field-level source csv. If none provided defaults to:\n"{default_field_source_url}"',
+    )
+
+    # Common data model table-level source url.
+    _ = parser.add_argument(
+        "--table-source",
+        "-ts",
+        type=str,
+        help=f'Path or url to table-level source csv. If none provided defaults to:\n"{default_table_source_url}"',
     )
 
     _ = parser.add_argument(
@@ -133,66 +151,7 @@ def parse_cli_arguments() -> tuple[Path, Path]:
     else:
         output_dir.mkdir(parents=True, exist_ok=True)
 
-    # Check on url.
-    if is_url(args.source):
-        try:
-            source_path: Path = download_url_to_temp_file(args.source, args.output_dir)
-        except Exception as e:
-            parser.exit(1, f"Failed to download from {args.source}: {e}")
-    else:
-        source_path = Path(args.source)
-        if not source_path.exists():
-            parser.exit(1, f"Source file does not exist: {source_path}")
-
-    return source_path, output_dir
-
-
-def table_handler(table: Tag) -> list[OmopDocumentationContainer]:
-    """Take a table and returns a list of objects that represent the tables in the table."""
-    rows: list[Tag] = [_ensure_tag(div) for div in table.find_all("tr")]
-
-    return [row_handler(row) for row in rows]
-
-
-def row_handler(row: Tag) -> OmopDocumentationContainer:
-    """
-    Parses a row from a CDM documentation table into an OmopDocumentationContainer instance.
-
-    Take each row from a table and handle it, resulting in a object that can neatly
-    store how the CDM docs express each column.
-    """
-    cell_tags: list[Tag] = [_ensure_tag(cell) for cell in row.find_all("td")]
-
-    cells_raw: dict[str, str] = {
-        "cdm_field": cell_tags[0].get_text(),
-        "user_guide": cell_tags[1].get_text(),
-        "etl_conventions": cell_tags[2].get_text(),
-        "datatype": cell_tags[3].get_text(),
-        "required": cell_tags[4].get_text(),
-        "primary_key": cell_tags[5].get_text(),
-        "foreign_key": cell_tags[6].get_text(),
-        "foreign_key_table": cell_tags[7].get_text(),
-        "foreign_key_domain": cell_tags[8].get_text(),
-    }
-
-    # Remove dangling whitespace and newlines from parsed HTML
-    cells_stripped: dict[str, str] = {
-        k: v.replace("\n", "").strip().replace("“", "").replace("”", "") for k, v in cells_raw.items()
-    }
-
-    # Convert sentinels to booleans. Assign values to omop_documentation_container DataClass.
-    return OmopDocumentationContainer(
-        cdm_field=cells_stripped["cdm_field"],
-        user_guide=cells_stripped["user_guide"],
-        etl_conventions=cells_stripped["etl_conventions"],
-        datatype=cells_stripped["datatype"],
-        required=sentinel_to_bool(cells_stripped["required"]),
-        primary_key=sentinel_to_bool(cells_stripped["primary_key"]),
-        foreign_key=sentinel_to_bool(cells_stripped["foreign_key"]),
-        foreign_key_table=cells_stripped["foreign_key_table"],
-        foreign_key_domain=cells_stripped["foreign_key_domain"],
-    )
-
+    return output_dir, args.field_source, args.table_source
 
 def sentinel_to_bool(text: str) -> bool:
     """Convert sentinel strings to boolean"""
@@ -221,7 +180,7 @@ def create_table_dict(
     ] = {
         "models": [
             {
-                "name": table,
+                "name": table.lower(),  # Ensure table name is lowercase in YAML
                 "description": table_description,
                 "columns": [
                     omop_docs_to_dbt_config(table, doc_container)
@@ -231,19 +190,6 @@ def create_table_dict(
         ]
     }
     return table_dict
-
-
-def extract_table_description(table_handle: Tag) -> str:
-    """Get the table description"""
-    sibling_element: _AtMostOneElement = _ensure_tag(
-        table_handle.find("p", string="Table Description")
-    ).next_sibling
-    description_element: _AtMostOneElement = _ensure_navigable_string(
-        sibling_element
-    ).next_sibling
-    description: str = _ensure_tag(description_element).get_text()
-
-    return description.replace("\n", " ")
 
 
 def omop_docs_to_dbt_config(
@@ -258,7 +204,7 @@ def omop_docs_to_dbt_config(
     """
     column_config: dict[str, str | list[str | dict[str, dict[str, str]]]] = {
         "name": doc_container.cdm_field,
-        "description": doc_container.user_guide,
+        "description": '' if doc_container.user_guide == 'NA' else doc_container.user_guide.replace('\n', ' '),
         "data_type": doc_container.datatype,
     }
 
@@ -272,8 +218,8 @@ def omop_docs_to_dbt_config(
         tests.append("unique")
 
     if doc_container.foreign_key:
-        if doc_container.foreign_key_domain == "":
-            # Handle simpler cases first, where a domain is not constrained
+        if (doc_container.foreign_key_domain == "" or doc_container.foreign_key_domain == "NA") and (doc_container.foreign_key_class == "" or doc_container.foreign_key_class == "NA"):
+            # Handle simpler cases first, where domain/class is not constrained
             test: dict[str, dict[str, str]] = {
                 "relationships": {
                     "to": f"ref('{doc_container.foreign_key_table.lower()}')",
@@ -281,7 +227,7 @@ def omop_docs_to_dbt_config(
                 }
             }
             tests.append(test)
-        else:
+        elif doc_container.foreign_key_domain != "" and doc_container.foreign_key_domain != "NA":
             # Add constrained domain tests
             specific_test: dict[str, dict[str, str]] = {
                 "dbt_utils.relationships_where": {
@@ -289,6 +235,17 @@ def omop_docs_to_dbt_config(
                     "field": f"{doc_container.foreign_key_table.lower()}_id",
                     "from_condition": f"{doc_container.cdm_field} <> 0",
                     "to_condition": f"domain_id = '{doc_container.foreign_key_domain}'",
+                }
+            }
+            tests.append(specific_test)
+        else:
+            # Add constrained class tests
+            specific_test: dict[str, dict[str, str]] = {
+                "dbt_utils.relationships_where": {
+                    "to": f"ref('{doc_container.foreign_key_table.lower()}')",
+                    "field": f"{doc_container.foreign_key_table.lower()}_id",
+                    "from_condition": f"{doc_container.cdm_field} <> 0",
+                    "to_condition": f"concept_class_id = '{doc_container.foreign_key_class}'",
                 }
             }
             tests.append(specific_test)
@@ -337,102 +294,100 @@ def omop_docs_to_dbt_config(
     return column_config
 
 
-def _ensure_tag(element: _AtMostOneElement) -> Tag:
-    """
-    Ensure that the given element is a BeautifulSoup Tag.
-
-    If the element is not a Tag, raises a ValueError.
-    """
-    if isinstance(element, Tag):
-        return element
-    raise ValueError(
-        f"No Tag returned from BeautifulSoup query.\nReturn type is {type(element)}"
-    )
-
-
-def _ensure_navigable_string(element: _AtMostOneElement) -> NavigableString:
-    """
-    Ensure that the given element is a BeautifulSoup NavigableString.
-
-    If the element is not a NavigableString, raises a ValueError.
-    """
-    if isinstance(element, NavigableString):
-        return element
-    raise ValueError(
-        f"No NavigableString returned from BeautifulSoup query.\nReturn type is {type(element)}"
-    )
+def parse_csv_file(file_path: Path) -> list[OmopDocumentationContainer]:
+    """Parse the CSV file and return a list of OmopDocumentationContainer objects."""
+    containers = []
+    with open(file_path, mode="r", encoding="utf-8-sig") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            containers.append(
+                OmopDocumentationContainer(
+                    table_name=row["cdmTableName"].strip(),
+                    cdm_field=row["cdmFieldName"].strip().replace('"', ''),
+                    user_guide=row["userGuidance"].strip(),
+                    etl_conventions=row["etlConventions"].strip(),
+                    datatype=row["cdmDatatype"].strip(),
+                    required=sentinel_to_bool(row["isRequired"].strip()),
+                    primary_key=sentinel_to_bool(row["isPrimaryKey"].strip()),
+                    foreign_key=sentinel_to_bool(row["isForeignKey"].strip()),
+                    foreign_key_table=row["fkTableName"].strip(),
+                    foreign_key_domain=row["fkDomain"].strip(),
+                    foreign_key_class=row["fkClass"].strip(),
+                )
+            )
+    return containers
 
 
-def extract_omop_table_names(soup_obj: BeautifulSoup) -> list[str]:
-    """Dynamically extract table names from the OMOP CDM documentation"""
-    headers: list[Tag] = [
-        _ensure_tag(div)
-        for div in soup_obj.find_all(
-            "div", attrs={"class": "section level3 tabset tabset-pills"}
-        )
-    ]
+def parse_table_descriptions(file_path: Path) -> dict[str, str]:
+    """Parse the CSV file containing table descriptions and return a dictionary mapping table names to descriptions."""
+    table_descriptions = {}
+    with open(file_path, mode="r", encoding="utf-8-sig") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            table_name = row["cdmTableName"].strip().lower()
+            description = row["tableDescription"].strip()
+            table_descriptions[table_name] = description
+    return table_descriptions
 
-    table_names: list[str] = []
-    for div in headers:
-        if div.h3:
-            table_names.append(div.h3.get_text())
 
-    # Exclude unwanted tables
-    unwanted_tables: set[str] = {"cohort", "cohort_definition"}
-    filtered_table_names: list[str] = [
-        table for table in table_names if table not in unwanted_tables
-    ]
-    return filtered_table_names
-
+def resolve_source_path(source: str, output_dir: Path) -> Path:
+    """Resolve the source path by downloading if it's a URL or validating if it's a local file."""
+    if is_url(source):
+        try:
+            return download_url_to_temp_file(source, output_dir)
+        except Exception as e:
+            raise ValueError(f"Failed to download from {source}: {e}")
+    else:
+        source_path = Path(source)
+        if not source_path.exists():
+            raise ValueError(f"Source file does not exist: {source_path}")
+        return source_path
 
 def main(
-    source_path: Path,
+    field_source: str,
+    table_source: str,
     output_dir: Path,
 ) -> None:
     """Main loop to generate dbt YAML files from the OMOP CDM documentation"""
-    with open(source_path) as file_handle:
-        file: str = file_handle.read()
+    field_source_path = resolve_source_path(field_source, output_dir)
+    containers = parse_csv_file(field_source_path)
 
-    soup: BeautifulSoup = BeautifulSoup(file, features="html.parser")
+    table_source_path = resolve_source_path(table_source, output_dir)
+    table_descriptions = parse_table_descriptions(table_source_path)
 
-    tables: list[str] = extract_omop_table_names(soup)
-    print(f" Found {len(tables)} tables in the OMOP CDM documentation")
+    tables = {container.table_name for container in containers}
 
-    for table in tables:
-        # For each table generate the desired dbt yaml
-        # Get desired div with table
-        div_handle: Tag = _ensure_tag(soup.find("div", attrs={"id": table}))
+    unwanted_tables: set[str] = {"cohort", "cohort_definition", "cohort_attribute"}
+    filtered_table_names: list[str] = [
+        table for table in tables if table.lower() not in unwanted_tables
+    ]
 
-        table_handle: Tag = _ensure_tag(div_handle.find("table"))
-        tbody_handle: Tag = _ensure_tag(table_handle.find("tbody"))
-        parsed_table: list[OmopDocumentationContainer] = table_handler(tbody_handle)
+    print(f" Found {len(tables)} tables in the OMOP CDM documentation, filtered to {len(filtered_table_names)} tables.")
 
-        table_description: str = extract_table_description(div_handle)
+    for table in filtered_table_names:
+        table_containers = [c for c in containers if c.table_name == table]
+        table_description = table_descriptions.get(table.lower(), '')
+        if table_description == 'NA':
+            table_description = ''
 
-        # TODO: Look at turning this structure into a dataclass or pydantic BaseModel class.
-        table_dict: dict[
-            str,
-            list[
-                dict[
-                    str,
-                    str | list[dict[str, str | list[str | dict[str, dict[str, str]]]]],
-                ]
-            ],
-        ] = create_table_dict(table, table_description, parsed_table)
+        table_dict = create_table_dict(table, table_description, table_containers)
 
         yaml: YAML = YAML()
         yaml.indent(mapping=2, sequence=4, offset=2)  # pyright: ignore[reportUnknownMemberType]
         yaml.width = 100
-        yaml.dump(table_dict, open(f"{output_dir}/{table}.yml", "w"))  # pyright: ignore[reportUnknownMemberType]
+        yaml.dump(table_dict, open(f"{output_dir}/{table.lower()}.yml", "w"))
         yaml.allow_duplicate_keys
 
-        if source_path.suffix == ".tmp" and source_path.exists():
-            source_path.unlink()
+        if field_source_path.suffix == ".tmp" and field_source_path.exists():
+            field_source_path.unlink()
+
+        if table_source_path.suffix == ".tmp" and table_source_path.exists():
+            table_source_path.unlink()
 
     print(f" Exported to `{output_dir}`")
     print("  Done!")
 
 
 if __name__ == "__main__":
-    source, output_dir = parse_cli_arguments()
-    main(source, output_dir)
+    output_dir, field_source, table_source = parse_cli_arguments()
+    main(field_source, table_source, output_dir)
