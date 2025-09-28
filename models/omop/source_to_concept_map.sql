89188d139638a3c6fdffe02d5d178300d92b8671
Katy Sadowski <katy.h.sadowski@gmail.com>
2025-09-28 19:48:40 -0400

    Adjust unit thresh. and fix concept check (#150)
    * adjust unit thresh. and fix concept check
    
    * fix yml
    
    ---------
    
    Co-authored-by: Katy Sadowski <katy.sadowski@gmail.com>

--- models/omop/source_to_concept_map.sql
+++ models/omop/source_to_concept_map.sql
@@ -1,11 +1,11 @@
 SELECT
-    {{ dbt.cast("null", api.Column.translate_type("varchar")) }} AS source_code
-    , {{ dbt.cast("null", api.Column.translate_type("integer")) }} AS source_concept_id
-    , {{ dbt.cast("null", api.Column.translate_type("varchar")) }} AS source_vocabulary_id
-    , {{ dbt.cast("null", api.Column.translate_type("varchar")) }} AS source_code_description
-    , {{ dbt.cast("null", api.Column.translate_type("integer")) }} AS target_concept_id
-    , {{ dbt.cast("null", api.Column.translate_type("varchar")) }} AS target_vocabulary_id
-    , {{ dbt.cast("null", api.Column.translate_type("date")) }} AS valid_start_date
-    , {{ dbt.cast("null", api.Column.translate_type("date")) }} AS valid_end_date
-    , {{ dbt.cast("null", api.Column.translate_type("varchar")) }} AS invalid_reason
-WHERE 1 = 0
+    source_code
+    , source_concept_id
+    , source_vocabulary_id
+    , source_code_description
+    , target_concept_id
+    , target_vocabulary_id
+    , valid_start_date
+    , valid_end_date
+    , invalid_reason
+FROM {{ ref('stg_vocabulary__source_to_concept_map') }}
